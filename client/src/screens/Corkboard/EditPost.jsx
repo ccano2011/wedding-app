409407bf32ebc5d990d7dbafa5537e329df43e1d
Chris Cano <chriscano@Chriss-MacBook-Pro.local>
2020-12-02 23:14:28 -0500

    EditPost.jsx reference code added

--- client/src/screens/Corkboard/EditPost.jsx
+++ client/src/screens/Corkboard/EditPost.jsx
@@ -1,11 +1,49 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
 
 function EditPost(props) {
-    return (
-        <div>
+    const [formData, setFormData] = useState({
+        name: ''
+    })
+    const { id } = useParams();
 
-        </div>
-    );
+    useEffect(() => {
+        const prefillForm = () => {
+            const foodItem = props.foods.find(food => food.id === Number(id));
+            setFormData({
+                name: foodItem.name
+            })
+        }
+        if (props.foods.length) {
+            prefillForm();
+        }
+    }, [props.foods])
+
+    const handleChange = (e) => {
+        const { name, value } = e.target;
+        setFormData(prevState => ({
+            ...prevState,
+            [name]: value
+        }))
+    }
+
+    return (
+        <form onSubmit={(e) => {
+            e.preventDefault();
+            props.handleUpdate(id, formData);
+        }}>
+            <h3>Edit Food</h3>
+            <label>Name:
+        <input
+                    type='text'
+                    name='name'
+                    value={formData.name}
+                    onChange={handleChange}
+                />
+            </label>
+            <button>Submit</button>
+        </form>
+    )
 }
 
 export default EditPost;
