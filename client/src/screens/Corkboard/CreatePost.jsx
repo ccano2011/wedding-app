fd7e1b4822258c2ee742c763057dd54185ddfeba
Chris Cano <chriscano@Chriss-MacBook-Pro.local>
2020-12-02 22:45:06 -0500

    'Createpost.jsx' successfully posts to the backend

--- client/src/screens/Corkboard/CreatePost.jsx
+++ client/src/screens/Corkboard/CreatePost.jsx
@@ -1,9 +1,9 @@
 import React, { useState } from 'react';
-import { Switch, Route, useHistory } from 'react-router-dom';
+import { createPost } from '../../services/posts'
+import { Redirect, Link, useHistory } from 'react-router-dom';
 
 function CreatePost(props) {
     const history = useHistory()
-
     console.log(props.currentUser)
     setTimeout(() => {
         if (props.currentUser === null) {
@@ -11,14 +11,54 @@ function CreatePost(props) {
         }
     }, 1)
 
-    const [post, setPost] = useState({
+    const [isCreated, setCreated] = useState(false)
+
+    const [newPost, setNewPost] = useState({
         content: '',
         name: ''
     })
 
+    const handleSubmit = async (e) => {
+        e.preventDefault()
+        const created = await createPost(newPost)
+        setCreated({ created })
+    }
+
+    const handleChange = (event) => {
+        const { name, value } = event.target
+        setNewPost({
+            ...newPost,
+            [name]: value
+        })
+    }
+
+    if (isCreated) {
+        return <Redirect to={`/corkboard`} />
+    }
+
     return (
         <div>
             <h2>TESTING CREATPOST COMPONENT ROUTE</h2>
+            <form className="create-form" onSubmit={handleSubmit}>
+                <textarea
+                    className="postBody"
+                    rows={10}
+                    placeholder='Your message to the bride & groom'
+                    value={newPost.content}
+                    name='content'
+                    required
+                    onChange={handleChange}
+                />
+                <input
+                    className="input-image-link"
+                    placeholder='Your Name'
+                    value={newPost.name}
+                    name='name'
+                    required
+                    onChange={handleChange}
+                />
+                <button type='submit' className="submit-button">Submit</button>
+            </form>
         </div>
     );
 }
--- client/src/services/posts.js
+++ client/src/services/posts.js
@@ -8,7 +8,22 @@ export const getAllPosts = async () => {
 }
 
 //Is this compatible with the backend association???? Find out next week on Dragonball Z!
-export const createPost = async (postId, userId) => {
-    const resp = await api.put(`/posts/${postId}/user/${userId}`);
+// export const createPost = async (postId, userId) => {
+//     const resp = await api.put(`/posts/`);
+//     return resp.data;
+// }
+
+export const createPost = async (data) => {
+    const resp = await api.post('/posts', { post: data });
     return resp.data;
 }
+
+// export const putFood = async (id, foodData) => {
+//     const resp = await api.put(`/foods/${id}`, { food: foodData });
+//     return resp.data;
+// }
+
+// export const destroyFood = async (id) => {
+//     const resp = await api.delete(`/foods/${id}`);
+//     return resp;
+// }
