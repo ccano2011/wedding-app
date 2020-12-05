5e3138c531dac4db252a184f4739d8bfbca9ac36
Chris Cano <chriscano@Chriss-MacBook-Pro.local>
2020-12-05 14:44:39 -0500

    intial createRSVP function created

--- client/src/screens/RSVP/RSVP.jsx
+++ client/src/screens/RSVP/RSVP.jsx
@@ -1,14 +1,44 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Nav from '../../shared/Nav'
 import { createRSVP } from '../../services/rsvp'
+// import { Redirect, Link, useHistory } from 'react-router-dom';
+
 
 
 
 function RSVP(props) {
+    const [isCreated, setCreated] = useState(false)
+
+    const [newRSVP, setNewRSVP] = useState({
+        firstname: '',
+        lastname: '',
+        entree: ''
+    })
+
+    const handleSubmit = async (e) => {
+        e.preventDefault()
+        const created = await createRSVP(newRSVP)
+        setCreated({ created })
+    }
+
+    const handleChange = (event) => {
+        const { name, value } = event.target
+        setNewRSVP({
+            ...newRSVP,
+            [name]: value
+        })
+    }
+
+    if (isCreated) {
+        alert('RSVP Created')
+        // return <Redirect to={`/corkboard`} />
+    }
     return (
-        <div>
-            <Nav />
-        </div>
+        <>
+            <div>
+                <Nav />
+            </div>
+        </>
     );
 }
 
