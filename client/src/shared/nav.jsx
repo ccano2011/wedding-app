0de0e3abddf480076f0d08b771d86ea489f4c08c
Chris Cano <chriscano@Chriss-MacBook-Pro.local>
2020-12-05 17:29:33 -0500

    beginning Nav.css

--- client/src/shared/nav.jsx
+++ client/src/shared/nav.jsx
@@ -1,20 +1,22 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import './Nav.css'
 
 function Nav(props) {
     return (
         <>
-            <div>
+            <div className='nav-title'>
                 <Link to="/">William & Clarissa</Link>
             </div>
-            <div>
-                <ul>
-                    <Link to='/our-story'><li>Our Story</li></Link>
-                    <Link to='/travel'><li>Travel</li></Link>
-                    <Link to='/pictures'><li>Pictures</li></Link>
-                    <Link to='/corkboard'><li>Cork Board</li></Link>
-                    <Link to='/registry'><li>Registry</li></Link>
-                    <Link to='/RSVP'><li>RSVP</li></Link>
+            <hr />
+            <div className="nav-ul-div">
+                <ul className='nav-ul'>
+                    <Link className='nav-links' to='/our-story'><li>Our Story</li></Link>
+                    <Link className='nav-links' to='/travel'><li>Travel</li></Link>
+                    <Link className='nav-links' to='/pictures'><li>Pictures</li></Link>
+                    <Link className='nav-links' to='/corkboard'><li>Cork Board</li></Link>
+                    <Link className='nav-links' to='/registry'><li>Registry</li></Link>
+                    <Link className='nav-links' to='/RSVP'><li>RSVP</li></Link>
                 </ul>
             </div>
         </>
