c589ac2c97da61aca55ecdcf681b99f5167629f8
Chris Cano <ccano2011@gmail.com>
2021-05-07 14:58:01 -0400

    Finished desktop media query on desktop

--- client/src/screens/OurStory/OurStory.jsx
+++ client/src/screens/OurStory/OurStory.jsx
@@ -6,74 +6,59 @@ import pic4 from "../../Assets/slidepic5.JPG"
 import styled from 'styled-components';
 
 const OurStoryWrapper = styled.div`
-/* max-height: auto; */
-/* background-color:rgba(255,250,240, 0.9);
-display:flex;
-flex-direction: row-reverse;
-justify-content: space-around;
-max-width:1672px;
-height:100vh;
-margin:auto; */
-background-color: rgba(255,250,240,0.9);
     display: flex;
-    /* flex-direction: row-reverse; */
-    /* -webkit-justify-content: space-around; */
-    /* justify-content: space-around; */
-    /* max-width: 1672px; */
     height: 100vh;
-@media (max-aspect-ratio: 0.79/1){
+    width:fit-content;
+    flex-direction: row-reverse;
+    justify-content: flex-end;
+    background-color: rgba(255,250,240,0.9);
+
+/* @media screen and (max-width: 768px){
     flex-direction: column;
     height: auto;
-}
-@media screen and (max-width: 768px){
-flex-direction: column;
-height: auto;
-}
+} */
 `
 const OurStoryTitle = styled.div`
-font-family: 'Pinyon Script', cursive;
-font-size:60px;
-padding-top:30vh;
-margin-left: 3vw;
-@media screen and (max-width: 768px){
+    padding-left: 5vw;
+    width: 100%;
+    padding-right: 45vw;
+    font-family: 'Pinyon Script', cursive;
+    font-size:60px;
+    padding-top:30vh;
+    @media screen and (min-width: 1400px){
+        padding-right: 35vw;
+    }
+    @media screen and (min-width: 1500px){
+        padding-left:10vw
+    }
+
+@media screen and (max-width: 808px){
     padding-top:0vh;
     font-size:40px;
 }
-@media screen and (max-height: 594px){
-    font-size:40px;
-}
-
 .ourStoryTitle{
     font-family: Arial, Helvetica, sans-serif;
     font-size: 20px;
-    max-width: 600px;
     margin: auto;
     line-height: 1.6;
+    padding-right: 10vw;
     padding-bottom: 5vh;
     @media screen and (max-width: 828px){
     font-size:16px;
+    max-width: 420px;
     }
-    @media screen and (max-height: 594px){
-    /* padding-top: 10vh; */
+    @media screen and (max-height: 760px){
     font-size:15px;
-}}
+}   }
 .title{
-    /* padding-left:3vw; */
     padding-bottom: 30px;
-    @media screen and (max-width: 768px){
-        text-align:center;
-        margin:auto;
-        font-size:40px;
-        margin-top: -70px;
-}}
+}
 `
+
 const OurStoryContent = styled.div`
 .photos{
-    margin-bottom: 10vh;
     height: 70vh;
-    margin-top: 40vh;
-    max-height: 698px;
-    /* margin-left: -108px; */
+    max-height: 550px;
     transition: opacity 550ms ease-in, -webkit-transform 400ms ease-in;
     transition: opacity 550ms ease-in, transform 400ms ease-in;
     transition: opacity 550ms ease-in, transform 400ms ease-in;
@@ -81,31 +66,32 @@ const OurStoryContent = styled.div`
     margin-right: 2vw; 
     padding: 10px;
     border: 2px solid #000;
-    @media screen and (max-width: 768px){
-    margin-top: 5vh;
-    height: auto;
-    width: 55vw;
-    display: flex;
-    margin-left: auto;
-    margin-right: auto;
-    opacity: 0;
-}}
+    @media screen and (max-width: 986px){
+    font-size:16px;
+    }
+}
 #stage {
-    margin-left: 9.5%;
-    margin-top: -180px;
-    width: 300px;
-    height:400px;
+    /* padding-left: 7.5%; */
+    /* margin-left:-7vw; */
   }
   #stage img {
     padding: 5px;
     border: 1px solid #ccc;
-    border-radius: 5%;
     background: whitesmoke;
     animation-name: fade; 
     animation-duration: 40s; 
     animation-iteration-count: infinite; 
-    position:absolute;
+    display: flex;
+    position: fixed;
+    top: 0;
+    right: 2%;
+    margin-right:6vw;
+    margin-top:23vh;
     opacity:0; 
+    @media screen and (min-width: 1500px){
+    margin-right:10vw;
+    }
+    
   }
 #stage img:nth-of-type(1) {
     animation-delay: 0s;
@@ -129,8 +115,8 @@ const OurStoryContent = styled.div`
 .photos.appear{
     opacity: 1;
 }
-
 `
+
 function OurStory() {
     const elemRef = useRef();
     useEffect(() => {
@@ -154,17 +140,16 @@ function OurStory() {
         <OurStoryWrapper id='our-story'>
             <OurStoryContent ref={elemRef} >
                 <div id="stage">
-                    {/* <img id="photos" src={image} alt="Bride & Groom" ref={elemRef} /> */}
                     <img className="photos" src={pic1} alt='' />
                     <img className="photos" src={pic2} alt='' />
                     <img className="photos" src={pic3} alt='' />
                     <img className="photos" src={pic4} alt='' />
                 </div>
+                <OurStoryTitle>
+                    <p className='title'>Our Story...</p>
+                    <p className='ourStoryTitle'>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+                </OurStoryTitle>
             </OurStoryContent>
-            <OurStoryTitle>
-                <p className='title'>Our Story...</p>
-                <p className='ourStoryTitle'>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-            </OurStoryTitle>
         </OurStoryWrapper>
     );
 }
